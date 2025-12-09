60d6cfd3d65ed911b9be06d41215999313536f73
vxtxcx <victorvicencio932@gmail.com>
2025-12-09 03:25:42 -0300

    forma1
    form1

--- authentication/views.py
+++ authentication/views.py
@@ -6,11 +6,21 @@ from rest_framework.authtoken.models import Token
 from rest_framework import status
 from empleado.models import Empleado
 from usuario.models import Usuario
+from django.shortcuts import get_object_or_404
 # Create your views here.
 
 @api_view(['POST'])
 def login(request):
-    return Response({})
+
+    user = get_object_or_404(User, username=request.data['username'])
+
+    if not user.check_password(request.data['password']):
+        return Response({"error": "Contraseña invalida"}, status=status.HTTP_400_BAD_REQUEST)
+    
+    token, created = Token.objects.get_or_create(user=user)
+    serializer = UserSerializer(instance=user)
+
+    return Response({"token": token.key, "user": serializer.data}, status=status.HTTP_200_OK)
 
 
 @api_view(['POST'])
